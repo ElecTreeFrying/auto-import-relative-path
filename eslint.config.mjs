79a9214f920f22a40f3534f7662f1e1be147392f
John James G. Ermitaño <jjgermitano@gmail.com>
2025-03-22 20:23:05 +0800

    eslint updates

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,7 +20,7 @@ export default [{
             format: ["camelCase", "PascalCase"],
         }],
 
-        curly: "warn",
+        curly: ['error', 'multi-line'],
         eqeqeq: "warn",
         "no-throw-literal": "warn",
         semi: "warn",
